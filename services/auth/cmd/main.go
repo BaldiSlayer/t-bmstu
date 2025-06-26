aa80c57fb28e2cc9bd0676cf8c15bc91ec071924
BaldiSlayer <lisov.a2005@yandex.ru>
2025-06-27 02:15:37 +0300

    bugs

--- services/auth/cmd/main.go
+++ services/auth/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"os"
 	"time"
@@ -42,7 +43,16 @@ func main() {
 	}
 	defer logger.Sync()
 
-	pool, err := pgxpool.New(context.Background(), "")
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+	)
+
+	pool, err := pgxpool.New(context.Background(), dsn)
 	if err != nil {
 		logger.Panic("failed to connect to the database", zap.Error(err))
 
